4eb35088b39f192c7a140e51c423a03bb62d5753
chanopolez <81364616+chanopolez@users.noreply.github.com>
2021-07-01 10:54:36 -0300

    Add files via upload

---
+++ Desarrollo 29.06 - Ignacio López.sql
@@ -0,0 +1,15 @@
+USE examen;
+
+INSERT INTO estudiantes VALUES
+('1','Juan','Perez','8','1A'),
+('2','Sol','Gonzalez','10','2B'),
+('3','Daniel','Aramburu','6','3A'),
+('4','Jose','Lopez','4','1B'),
+('5','Marta','Caceres','5','2A'),
+('6','Ivan','Suarez','7','5A'),
+('7','Ruben','Pasteur','3','5B'),
+('8','Pablo','Pagani','9','6B'),
+('9','Susana','Romero','8','6A'),
+('10','Maria','Lujan','10','4A');
+
+SELECT * FROM estudiantes;
